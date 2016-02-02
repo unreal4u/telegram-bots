98a6da11f0564c5fbd65242c018df5738fb09d52
Camilo Sperberg <timebot@telegram.unreal4u.com>
2016-02-02 23:30:24 +0100

    Finished basic model of TheTimeBot and unreal4uBot

--- src/Bots/TheTimeBot.php
+++ src/Bots/TheTimeBot.php
@@ -2,6 +2,14 @@
 
 namespace unreal4u\Bots;
 
+use unreal4u\Telegram\Types\Update;
+use unreal4u\Abstracts\TelegramTypes;
+use unreal4u\Telegram\Types\Chat;
+use unreal4u\Telegram\Methods\SendMessage;
+use unreal4u\TgLog;
+use unreal4u\localization;
+use Psr\Log\LoggerInterface;
+
 class TheTimeBot implements BotsInterface
 {
     /**
@@ -13,15 +21,111 @@ class TheTimeBot implements BotsInterface
      */
     private $token;
 
+    protected $command = '';
+
+    protected $arguments = '';
+
     public function __construct(LoggerInterface $logger, string $token)
     {
-
         $this->logger = $logger;
         $this->token = $token;
     }
 
     public function run(array $postData=[])
     {
+        $update = new Update($postData, $this->logger);
+        $this->logger->debug('Incoming data', $postData);
+        $this->performAction($update);
+	return $this;
+    }
 
+    public function performAction(Update $update) 
+    {
+        if (!empty($update->message->text)) {
+            $spacePosition = strpos($update->message->text, ' ');
+            if ($spacePosition === false) {
+                $spacePosition = strlen($update->message->text);
+            } else {
+                $this->arguments = substr($update->message->text, $spacePosition + 1);
+            }
+            $this->command = trim(substr($update->message->text, 1, $spacePosition));
+            $this->logger->info(sprintf('The requested command is "%s". Arguments are "%s"', $this->command, $this->arguments));
+        }
+
+        $sendMessage = $this->prepareUserMessage($this->constructBasicMessage(), $update->message->chat);
+        $this->sendToUser($sendMessage);
+
+        return $this;
+    }
+
+    protected function constructBasicMessage(): string
+    {
+        switch ($this->command) {
+            case 'start':
+                $messageText = 'Welcome! Consult /help to get a list of command and options.';
+                break;
+            case 'help':
+                $messageText = 'Example commands:'.PHP_EOL;
+                $messageText .= '`/get_time_for_timezone America/Santiago` -> Displays the current time in America/Santiago'.PHP_EOL;
+                $messageText .= '`/set_display_format en-US` -> Sets the display format, use a valid locale'.PHP_EOL;
+                break;
+            case 'get_time_for_timezone':
+                try {
+                    $messageText = sprintf('The time in %s is now %s', $this->arguments, $this->getTheTime());
+                    $this->logger->info(sprintf('"%s" is a valid timezone, sending information back to user', $this->arguments));
+                } catch (\Exception $e) {
+                    $this->logger->warning('Invalid timezone detected', ['timezone' => $this->arguments]);
+                    $messageText = sprintf(
+                        'Sorry but "%s" is not a valid timezone identifier. Please check %s for all possible timezone identifiers',
+                        $this->arguments,
+                        'http://php.net/manual/en/timezones.php'
+                    );
+                }
+                break;
+            case 'set_display_format':
+                $messageText = 'Sorry but this command is not yet implemented, check later!';
+                break;
+            default:
+                $this->logger->warning('Invalid command detected', ['command' => $this->command, 'arguments' => $this->arguments]);
+                $messageText = 'Sorry but I don\'t understand this option, please check /help';
+                break;
+        }
+
+        return $messageText;
+    }
+
+    protected function sendToUser(SendMessage $sendMessage): TelegramTypes
+    {
+        $this->logger->debug('Sending the message to user');
+        $tgLog = new TgLog($this->token, $this->logger);
+        return $tgLog->performApiRequest($sendMessage);
+    }
+
+    protected function prepareUserMessage(string $messageText, Chat $chat): SendMessage
+    {
+        $this->logger->debug('Preparing the actual message to be sent to the user', ['text' => $messageText, 'chatId' => $chat->id]);
+        $sendMessage = new SendMessage();
+        $sendMessage->chat_id = $chat->id;
+        $sendMessage->text = $messageText;
+        $sendMessage->parse_mode = 'Markdown';
+
+        return $sendMessage;
+    }
+
+    protected function getTheTime(): string
+    {
+        $this->logger->debug(sprintf('Calculating the time for timezone "%s"', $this->arguments));
+        $theTime = '';
+
+        $localization = new localization();
+        $acceptedTimezone = $localization->setTimezone($this->arguments);
+
+        if ($acceptedTimezone == $this->arguments) {
+            $theTime = $localization->formatSimpleDate(0, $acceptedTimezone).' '.$localization->formatSimpleTime(0, $acceptedTimezone);
+        } else {
+            throw new \Exception('Invalid timezone, please try again');
+        }
+
+        return $theTime;
     }
 }
--- src/Bots/unreal4uBot.php
+++ src/Bots/unreal4uBot.php
@@ -3,6 +3,7 @@
 namespace unreal4u\Bots;
 
 use unreal4u\Telegram\Types\Update;
+use unreal4u\Telegram\Types\Message;
 use unreal4u\Telegram\Types\InlineQueryResultArticle;
 use unreal4u\Telegram\Methods\AnswerInlineQuery;
 use unreal4u\TgLog;
@@ -28,48 +29,80 @@ class unreal4uBot implements BotsInterface
     public function run(array $postData = [])
     {
         try {
-            $update = new Update($_POST);
+            $update = new Update($postData, $this->logger);
             $this->logger->debug('Incoming post data', $_POST);
-            if (!empty($update->message->chat->id)) {
-                $this->logger->info(sprintf(
-                    'Received message. Chat id: %d, user id: %d, user name: %s',
-                    $update->message->chat->id,
-                    $update->message->from->id,
-                    $update->message->from->username
-                ));
-            }
-            if (!empty($update->chosen_inline_result)) {
-                $this->logger->debug(print_r($update->chosen_inline_result, true));
-            }
-            if (!empty($update->inline_query)) {
-                $this->logger->info(sprintf(
-                    'Received inline query. User id %d (username: %s). Query: "%s", inline query id: %s',
-                    $update->inline_query->from->id,
-                    $update->inline_query->from->username,
-                    $update->inline_query->query,
-                    $update->inline_query->id
-                ));
-                $query = $update->inline_query->query;
-                if (empty($query)) {
-                    $query = 'What is lmgtfy?';
-                }
-                $inlineQueryResultArticle = new InlineQueryResultArticle();
-                $inlineQueryResultArticle->url = 'http://lmgtfy.com/?q=' . urlencode($query);
-                $inlineQueryResultArticle->title = $inlineQueryResultArticle->url; //'Forward this message to anyone you would like (Title)';
-                $inlineQueryResultArticle->message_text = $inlineQueryResultArticle->url; //'Forward this message to anyone you would like (Message)';
-                $inlineQueryResultArticle->disable_web_page_preview = true;
-                $inlineQueryResultArticle->hide_url = true;
-                $inlineQueryResultArticle->id = md5(uniqid());
-                $answerInlineQuery = new AnswerInlineQuery();
-                $answerInlineQuery->inline_query_id = $update->inline_query->id;
-                $answerInlineQuery->results[] = $inlineQueryResultArticle;
-                $tgLog = new TgLog($this->token);
-                //$tgLog->logger = $this->logger;
-                $tgLog->performApiRequest($answerInlineQuery);
-                $this->logger->info(sprintf('Sent API response to Telegram, all done'));
-            }
+            $this->performAction($update);
         } catch (\Exception $e) {
             $this->logger->error(sprintf('Captured exception: "%s"', $e->getMessage()));
         }
+
+        return $this;
+    }
+
+    public function performAction(Update $update)
+    {
+        if (!empty($update->message->chat->id)) {
+            $this->handleIncomingMessage($update->message);
+        }
+
+        if (!empty($update->chosen_inline_result)) {
+            $this->logger->debug('We have a chosen_inline_result back, result id: '.$update->chosen_inline_result->result_id);
+        }
+
+
+        if (!empty($update->inline_query)) {
+            $this->inlineQuery($update);
+        }
+
+        return $this;
+    }
+
+    public function handleIncomingMessage(Message $message): unreal4uBot
+    {
+        $this->logger->info(sprintf(
+            'Received message. Chat id: %d, user id: %d, user name: %s',
+            $message->chat->id,
+            $message->from->id,
+            $message->from->username
+        ));
+
+        return $this;
+    }
+
+    public function inlineQuery(Update $update): unreal4uBot
+    {
+        $this->logger->info(sprintf(
+            'Received inline query. User id %d (username: %s). Query: "%s", inline query id: %s',
+            $update->inline_query->from->id,
+            $update->inline_query->from->username,
+            $update->inline_query->query,
+            $update->inline_query->id
+        ));
+        $query = $update->inline_query->query;
+        if (empty($query)) {
+            $query = 'What is lmgtfy?';
+        }
+
+        // Number of results
+        $i = 1;
+
+        $inlineQueryResultArticle = new InlineQueryResultArticle();
+        $inlineQueryResultArticle->url = 'http://lmgtfy.com/?q=' . urlencode($query);
+        $inlineQueryResultArticle->title = $inlineQueryResultArticle->url; //'Forward this message to anyone you would like (Title)';
+        $inlineQueryResultArticle->message_text = $inlineQueryResultArticle->url; //'Forward this message to anyone you would like (Message)';
+        $inlineQueryResultArticle->disable_web_page_preview = true;
+        $inlineQueryResultArticle->hide_url = true;
+        // @TODO find a way to compress this all into an identifiable 64bit ascii string, maybe with pack()?
+        $inlineQueryResultArticle->id = md5(json_encode(['uid' => $update->inline_query->from->id, 'iqid' => $update->inline_query->id, 'rid' => $i]));
+        $answerInlineQuery = new AnswerInlineQuery();
+        $answerInlineQuery->inline_query_id = $update->inline_query->id;
+        $answerInlineQuery->results[] = $inlineQueryResultArticle;
+
+        $tgLog = new TgLog($this->token, $this->logger);
+        //$tgLog->logger = $this->logger;
+        $tgLog->performApiRequest($answerInlineQuery);
+        $this->logger->info(sprintf('Sent API response to Telegram, all done'));
+
+        return $this;
     }
 }
