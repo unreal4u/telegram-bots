5e4b6e6d0dbf32936af0ae25212c29f512f6b09c
Camilo Sperberg <me@unreal4u.com>
2016-11-28 01:03:12 +0100

    Strip the actual slash from the request uri

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ $logger = new Logger('TGBot');
 $streamHandler = new StreamHandler('telegramApiLogs/main.log');
 $logger->pushHandler($streamHandler);
 
-$trimmedRequestUri = trim($_SERVER['REQUEST_URI']);
+$trimmedRequestUri = trim($_SERVER['REQUEST_URI'], '/');
 
 new RequestHandler($logger, $trimmedRequestUri);
